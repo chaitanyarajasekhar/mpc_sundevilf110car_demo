0767f7ff61c995668ff6ae9471e98da06ca12119
Chaitanya Rajasekhar <chaitanyar5atp@gmail.com>
2019-04-09 15:17:23 -0700

    addged pi function uncommented assert in polyfit

--- src/main.cpp
+++ src/main.cpp
@@ -99,6 +99,7 @@ ros::Time getTime(){
 /*************************************************************************************/
 // functions to convert from degrees to radians and vice versa
 
+constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
@@ -129,7 +130,7 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
 Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
                         int order) {
   assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
+  //assert(order >= 1 && order <= xvals.size() - 1);
   Eigen::MatrixXd A(xvals.size(), order + 1);
 
   for (int i = 0; i < xvals.size(); i++) {
